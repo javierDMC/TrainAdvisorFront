8d79c297b0b6a6ccab25c82afaec799c83e32cf0
javierDMC <javierdelmoralcamacho@gmail.com>
2023-05-11 23:16:54 +0200

    Router link portada

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,13 +4,14 @@
     <header>
     <nav class="bg-lime-500 border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://marca.com" class="flex items-center">
-                <img src="./../../public/travel-holiday-vacation-301_89079.svg" class="mr-3 h-6 sm:h-9" alt="TrainAdvisor Logo" />
+            <router-link to="/"><a class="flex items-center"><img src="./../../public/travel-holiday-vacation-301_89079.svg" class="mr-3 h-6 sm:h-9" alt="TrainAdvisor Logo" />
                 <span class=" text-xl font-semibold whitespace-nowrap dark:text-white">Train Advisor</span>
-            </a>
+            </a></router-link>
             <div class="flex items-center lg:order-2">
-                <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Registro</a>
-                <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Inciar sesión</a>
+                <router-link to="/login"><a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Registro</a>
+</router-link>
+                <router-link to="/registro"><a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Inciar sesión</a>
+</router-link>
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
